fafe2701d84564d3f0c902e2f6479dd3e6f6a282
mmfvc <98354615+mmfvc@users.noreply.github.com>
2023-03-28 19:26:04 +0300

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -6,8 +6,8 @@
 global botkey
 global qiwi_token
 
-botkey = ''
-owners_id = ['']
+botkey = '6168801029:AAHur8TbhnMJDI7UGm2wDxfzJ5oYNqv-FNc'
+owners_id = ['5602778747']
 qiwi_token = ""
 
 logToFile = True
